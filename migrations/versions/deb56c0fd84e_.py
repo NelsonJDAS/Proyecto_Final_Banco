f98bc74ec255383d6171e40a044afe94c5dc1d38
Nelson De Abreu <141588178+NelsonJDAS@users.noreply.github.com>
2024-12-19 20:47:58 +0000

    Endpoint test and Endpoint login added

--- migrations/versions/deb56c0fd84e_.py
+++ migrations/versions/deb56c0fd84e_.py
@@ -1,8 +1,8 @@
 """empty message
 
-Revision ID: 9b228af1876d
+Revision ID: deb56c0fd84e
 Revises: 
-Create Date: 2024-12-19 20:08:08.671526
+Create Date: 2024-12-19 20:28:38.845871
 
 """
 from alembic import op
@@ -10,7 +10,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision = '9b228af1876d'
+revision = 'deb56c0fd84e'
 down_revision = None
 branch_labels = None
 depends_on = None
--- src/api/routes.py
+++ src/api/routes.py
@@ -12,19 +12,41 @@ api = Blueprint('api', __name__)
 # Allow CORS requests to this API
 CORS(api)
 
+
+@api.route('/test')
+def test():
+    es_divisible_por_dos = lambda numero: True if numero % 2 == 0 else False
+    for i in range(1, 101):
+        new_user = User(
+            email= f"ejemplo{i}",
+            password= f"ejemplo{i}",
+            is_active= es_divisible_por_dos(i)
+        )
+        db.session.add(new_user)
+    db.session.commit() 
+    
+    Users = User.query.all()
+    return jsonify([user.serialize() for user in Users]), 201
+
+
 @api.route('/Users')
 def getUsers():
     Users = User.query.all()
-    return jsonify([user.serialize() for user in Users])
+    return jsonify([user.serialize() for user in Users]), 201
 
-@api.route('/addUser', methods=['POST'])
+@api.route('/User/Register', methods=['POST'])
 def addUser():
     data = request.get_json()
-    if data.get("email") is None or data.get("email") is "":
+    email = data.get("email")
+    password = data.get("password")
+    is_active = data.get("is_active")
+    
+    
+    if email is None or email is "":
         return jsonify({"Mensaje": "The email is missing"}), 400
-    elif data.get("password") is None or data.get("password") is "":
+    elif password is None or password is "":
         return jsonify({"Mensaje": "The password is missing"}), 400
-    elif data.get("is_active") is None or data.get("is_active") is "":
+    elif is_active is None or is_active is "":
         return jsonify({"Mensaje": "The is_active is missing"}), 400
     try:
         new_user = User(
@@ -46,11 +68,29 @@ def get_user(user_id):
         return jsonify({"Mensaje": "invalid user_id"}), 400
     try:
         user = User.query.filter_by(id=user_id).first()
-        return jsonify(user.serialize()), 200  
+        return jsonify(user.serialize()), 201  
     except Exception as e:
         return jsonify({"error": str(e)}), 400         
 
-
+@api.route('/User/Login', methods=['POST'])
+def user_autentication():
+    data = request.get_json()
+    email = data.get("email")
+    password = data.get("password")
+    if email is None or email is "":
+        return jsonify({"Mensaje": "The email is missing"}), 400
+    elif password is None or password is "":
+        return jsonify({"Mensaje": "The password is missing"}), 400
+    try:
+        user = User.query.filter_by(password=password, email=email).first()
+        
+        access_token = create_access_token(identity=data.get('id'))
+        if user is None:
+            return jsonify({"mensaje": "Invalid password or email"}), 400  
+        else:
+            return jsonify({"Usuario Identificado": user.serialize(),"token" : access_token}), 201    
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400         
 
     
     
