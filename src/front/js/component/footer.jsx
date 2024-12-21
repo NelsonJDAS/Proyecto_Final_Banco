3958fc60e03f41fdb0b1b2168f59a034ffeb8cb0
Angel Gallego <angelgb1990@gmail.com>
2024-12-21 17:53:41 +0000

    arreglos y importacion Footer

--- src/front/js/component/footer.jsx
+++ src/front/js/component/footer.jsx
@@ -11,37 +11,37 @@ export const Footer = () => {
 		<footer className="footer mt-auto py-3 bg-black border-top border-white">
 			<div className="row px-3 m-0">
 				<div className="col-2 text-start d-flex flex-column">
-					<h6 className="subtitulo text-white">{t('Contact information')}</h6>
-					<span className="enlace text-white">{t('Contact')}</span>
-					<span className="enlace text-white">{t('Chat')}</span>
+					<h6 className="subtitulo text-white">Informacion de contacto</h6>
+					<span className="enlace-claro text-white">Contacto</span>
+					<span className="enlace-claro text-white">Chat</span>
 				</div>
 				<div className="col-2 text-start d-flex flex-column">
-					<h6 className="subtitulo text-white">{t('Legal links')}</h6>
-					<span className="enlace text-white">{t('Privacy Policy')}</span>
-					<span className="enlace text-white">{t('Term of conditions')}</span>
-					<span className="enlace text-white">{t('Legal notice')}</span>
+					<h6 className="subtitulo text-white">Enlaces legales</h6>
+					<span className="enlace-claro text-white">Politica de privacidad</span>
+					<span className="enlace-claro text-white">Termino de condiciones</span>
+					<span className="enlace-claro text-white">aviso legal</span>
 				</div>
 				<div className="col-2 text-start d-flex flex-column">
-					<h6 className="subtitulo text-white">{t('Links to resources')}</h6>
-					<span className="enlace text-white">{t('Rates and commissions')}</span>
-					<span className="enlace text-white">{t('Education')}</span>
+					<h6 className="subtitulo text-white">Enlaces a recursos</h6>
+					<span className="enlace-claro text-white">Tarifas / Comisiones</span>
+					<span className="enlace-claro text-white">Educacion</span>
 				</div>
 				<div className="col-2 text-start d-flex flex-column">
-					<h6 className="subtitulo text-white">{t('Security')}</h6>
-					<span className="enlace text-white">{t('Tips')}</span>
-					<span className="enlace text-white">{t('Security methods')}</span>
-					<span className="enlace text-white">{t('Coordinate card')}</span>
+					<h6 className="subtitulo text-white">Seguridad</h6>
+					<span className="enlace-claro text-white">Consejos</span>
+					<span className="enlace-claro text-white">Metodos de Seguridad</span>
+					<span className="enlace-claro text-white">Tarjeta de cordenadas</span>
 				</div>
 				<div className="col-2 text-start d-flex flex-column">
-					<h6 className="subtitulo text-white">{t('Products and services')}</h6>
-					<span className="enlace text-white">{t('Shop')}</span>
-					<span className="enlace text-white">{t('Insurance')}</span>
-					<span className="enlace text-white">{t('Loans')}</span>
+					<h6 className="subtitulo text-white">Productos y Servicios</h6>
+					<span className="enlace-claro text-white">Store</span>
+					<span className="enlace-claro text-white">Seguro</span>
+					<span className="enlace-claro text-white">Prestamo</span>
 				</div>
 				<div className="col-2 text-start d-flex flex-column">
-					<h6 className="subtitulo text-white">{t('Certifications')}</h6>
-					<span className="enlace text-white">{t('Certified titles')}</span>
-					<span className="enlace text-white">{t('Security titles')}</span>
+					<h6 className="subtitulo text-white">Certificaciones</h6>
+					<span className="enlace-claro text-white">Titulos certificados</span>
+					<span className="enlace-claro text-white">Titulos de seguridad</span>
 				</div>
 			</div>
 		</footer>
