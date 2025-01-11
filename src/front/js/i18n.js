9ee6fd3bc525c336b096f9324414ab0cade76e98
Angel Gallego <angelgb1990@gmail.com>
2025-01-11 19:19:31 +0000

    agregada traduc act ualizadas

--- src/front/js/i18n.js
+++ src/front/js/i18n.js
@@ -4,10 +4,22 @@ import LanguageDetector from 'i18next-browser-languagedetector';
 
 import en from './locales/en.json';
 import es from './locales/es.json';
+import fr from './locales/fr.json';
+import pt from './locales/pt.json';
+import de from './locales/de.json';
+import ru from './locales/ru.json';
+import ch from './locales/ch.json';
+import ar from './locales/ar.json';
 
 const resources = {
   en: { translation: en },
   es: { translation: es },
+  fr: { translation: fr },
+  pt: { translation: pt },
+  de: { translation: de },
+  ru: { translation: ru },
+  ch: { translation: ch },
+  ar: { translation: ar }
 };
 
 i18n
@@ -16,7 +28,7 @@ i18n
   .init({
     resources,
     fallbackLng: 'en',
-    supportedLngs: ['en', 'es'],
+    supportedLngs: ['en', 'es', 'fr', 'pt', 'de', 'ru', 'ch', 'ar'],
     debug: true,
     interpolation: {
       escapeValue: false
