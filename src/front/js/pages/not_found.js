0cf9b4ba2c4bd5172fa3b9faad54e3901f6de72d
Nelson De Abreu <141588178+NelsonJDAS@users.noreply.github.com>
2025-01-16 16:29:36 +0000

    not found finished

--- src/front/js/pages/not_found.js
+++ src/front/js/pages/not_found.js
@@ -3,7 +3,5 @@ import "../../styles/not_found.css";
 import ContenidoPrincipal from "../component/not_found/ContenidoPrincipal.jsx";
 
 export const Not_found = () => {
-  return <></>;
+  return <ContenidoPrincipal />;
 };
-
-//<ContenidoPrincipal />
