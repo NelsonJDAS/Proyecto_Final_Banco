d808dcc576d994cee5f231f0c4d7b3dfcfdf098a
Nelson De Abreu <141588178+NelsonJDAS@users.noreply.github.com>
2025-01-17 18:52:08 +0000

    dev

---
+++ src/front/js/component/Individuales/politicas/ContenedorProteccion.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+const ContenedorProteccion = () => {
+    return (
+        <div className="container">
+            <h2 className="text-center">Protección de la Información</h2>
+            <p>
+                Implementamos medidas de seguridad avanzadas para proteger su
+                información contra accesos no autorizados, alteraciones, divulgaciones o
+                destrucción.
+            </p>
+        </div>
+    )
+}
+
+export default ContenedorProteccion
