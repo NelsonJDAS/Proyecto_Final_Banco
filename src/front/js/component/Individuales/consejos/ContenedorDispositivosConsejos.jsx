e84aba2422fca10ac0146fdf8e687cdb84929180
Alejandro <alejandrolujan1999@outlook.es>
2025-01-20 19:36:21 +0000

    vista consejos

---
+++ src/front/js/component/Individuales/consejos/ContenedorDispositivosConsejos.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const ContenedorDispositivosConsejos = () => {
+    return (
+        <div className="container">
+            <h1 className="text-center">Proteja sus Dispositivos</h1>
+            <p>
+                <ul>
+                    <li>Mantenga actualizado el software y los sistemas operativos de sus dispositivos.</li>
+                    <li>Instale programas antivirus y realice análisis regulares.</li>
+                    <li>Evite conectarse a redes Wi-Fi públicas para realizar transacciones bancarias.</li>
+                </ul>
+            </p>
+        </div>
+    )
+}
+
+export default ContenedorDispositivosConsejos
