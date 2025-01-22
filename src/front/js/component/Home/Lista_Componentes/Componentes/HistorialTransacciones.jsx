9c1dbe91ab464b3935aacaae15ae3f504af2f0bf
Nelson De Abreu <141588178+NelsonJDAS@users.noreply.github.com>
2025-01-22 19:34:46 +0000

    finished

--- src/front/js/component/Home/Lista_Componentes/Componentes/HistorialTransacciones.jsx
+++ src/front/js/component/Home/Lista_Componentes/Componentes/HistorialTransacciones.jsx
@@ -7,7 +7,7 @@ const HistorialTransacciones = () => {
     return (
         <div className={`bg-transferencias animacion-contenedor hover contenedor-componente-interactivo my-2 text-center fw-bold ${store.borde} text-dark d-flex flex-column`}>
             <p className="fs-1 objeto-animado"><BiTransfer /></p>
-            <p>Lista detallada</p>
+            <p>Movimientos</p>
             <p>con cada transferencia</p>
             <span className="align-content-start mb-auto my-2">Historial De Transacciones</span>
         </div >
