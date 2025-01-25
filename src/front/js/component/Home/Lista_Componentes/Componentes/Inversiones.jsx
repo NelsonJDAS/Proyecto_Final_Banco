4e14c40d98e9dc3eb32d24a1c071dba6a733eb30
Nelson De Abreu <141588178+NelsonJDAS@users.noreply.github.com>
2025-01-25 13:46:58 +0000

    finished

--- src/front/js/component/Home/Lista_Componentes/Componentes/Inversiones.jsx
+++ src/front/js/component/Home/Lista_Componentes/Componentes/Inversiones.jsx
@@ -1,15 +1,18 @@
 import React, { useContext } from "react";
 import { Context } from "../../../../store/appContext";
-import { IoIosChatboxes } from "react-icons/io";
 import GraficaComponenteInversiones from "../../../graficas/GraficaComponenteInversiones.jsx";
 import { RiBtcLine } from "react-icons/ri";
+import { useNavigate } from "react-router-dom";
 
 
 const Inversiones = () => {
     const { store, actions } = useContext(Context);
+    const navigate = useNavigate("")
 
     return (
-        <div className={`bg-dark animacion-contenedor hover contenedor-componente-interactivo my-2 text-center text-white fw-bold ${store.borde} text-white align-content-center`}>
+        <div className={`bg-dark animacion-contenedor hover contenedor-componente-interactivo my-2 text-center text-white fw-bold ${store.borde} text-white align-content-center`} onClick={() => {
+            navigate("/inversiones")
+        }}>
             <div className="row mt-2">
                 <div className="col-8 text-center"><p className="objeto-animado">Finance Geeks</p></div>
                 <div className="col-4 text-center"><p className="objeto-animado"><i><RiBtcLine /></i></p></div>
---
+++ src/front/js/pages/inversiones.js
@@ -0,0 +1,8 @@
+import React, { useContext } from "react";
+import "../../styles/inversiones.css";
+import { Context } from "../store/appContext";
+
+export const Inversiones = () => {
+    const { store, actions } = useContext(Context);
+    return <dvi>hola</dvi>;
+};
