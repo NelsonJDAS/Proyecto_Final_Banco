5c8dd24f5b33d018fed381430d65618a2c574840
Angel Gallego <angelgb1990@gmail.com>
2025-02-02 21:44:00 +0000

    antes de playwrigth

---
+++ src/amazon_scraper/amazon_scraper/items.py
@@ -0,0 +1,14 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+class AmazonScraperItem(scrapy.Item):
+    # asin = scrapy.Field()
+    title = scrapy.Field()
+    price = scrapy.Field()
+    image_url = scrapy.Field()
+    # rating = scrapy.Field()
+    # review_count = scrapy.Field()
