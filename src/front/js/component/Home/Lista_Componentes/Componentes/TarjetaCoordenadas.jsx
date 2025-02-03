0d2bd29b1f29bf79dd37c43da84b470a0faa1abc
Nelson De Abreu <141588178+NelsonJDAS@users.noreply.github.com>
2025-02-03 14:15:07 +0000

    coordenadas fixed

--- src/front/js/component/Home/Lista_Componentes/Componentes/TarjetaCoordenadas.jsx
+++ src/front/js/component/Home/Lista_Componentes/Componentes/TarjetaCoordenadas.jsx
@@ -184,29 +184,29 @@ const TarjetasCoordenadas = () => {
                         <div className="row">
                             <div className="col-4 text-center border-top border-dark"><p className={`fw-bold my-1 py-1 ${store.hidden ? "desenfoque" : ""}`}>1</p></div>
                             <div className="col-2 text-center border-top border-dark"><p className={`fw-bold my-1 py-1 ${store.hidden ? "desenfoque" : ""}`}>{datos[0]?.valor}</p></div>
-                            <div className="col-2 text-center border-top border-dark"><p className={`fw-bold my-1 py-1 ${store.hidden ? "desenfoque" : ""}`}>{datos[1]?.valor}</p></div>
-                            <div className="col-2 text-center border-top border-dark"><p className={`fw-bold my-1 py-1 ${store.hidden ? "desenfoque" : ""}`}>{datos[2]?.valor}</p></div>
-                            <div className="col-2 text-center border-top border-dark"><p className={`fw-bold my-1 py-1 ${store.hidden ? "desenfoque" : ""}`}>{datos[3]?.valor}</p></div>
+                            <div className="col-2 text-center border-top border-dark"><p className={`fw-bold my-1 py-1 ${store.hidden ? "desenfoque" : ""}`}>{datos[4]?.valor}</p></div>
+                            <div className="col-2 text-center border-top border-dark"><p className={`fw-bold my-1 py-1 ${store.hidden ? "desenfoque" : ""}`}>{datos[8]?.valor}</p></div>
+                            <div className="col-2 text-center border-top border-dark"><p className={`fw-bold my-1 py-1 ${store.hidden ? "desenfoque" : ""}`}>{datos[12]?.valor}</p></div>
                         </div>
                         <div className="row">
                             <div className="col-4 text-center border-top border-dark"><p className={`fw-bold my-1 py-1 ${store.hidden ? "desenfoque" : ""}`}>2</p></div>
-                            <div className="col-2 text-center border-top border-dark"><p className={`fw-bold my-1 py-1 ${store.hidden ? "desenfoque" : ""}`}>{datos[4]?.valor}</p></div>
+                            <div className="col-2 text-center border-top border-dark"><p className={`fw-bold my-1 py-1 ${store.hidden ? "desenfoque" : ""}`}>{datos[1]?.valor}</p></div>
                             <div className="col-2 text-center border-top border-dark"><p className={`fw-bold my-1 py-1 ${store.hidden ? "desenfoque" : ""}`}>{datos[5]?.valor}</p></div>
-                            <div className="col-2 text-center border-top border-dark"><p className={`fw-bold my-1 py-1 ${store.hidden ? "desenfoque" : ""}`}>{datos[6]?.valor}</p></div>
-                            <div className="col-2 text-center border-top border-dark"><p className={`fw-bold my-1 py-1 ${store.hidden ? "desenfoque" : ""}`}>{datos[7]?.valor}</p></div>
+                            <div className="col-2 text-center border-top border-dark"><p className={`fw-bold my-1 py-1 ${store.hidden ? "desenfoque" : ""}`}>{datos[9]?.valor}</p></div>
+                            <div className="col-2 text-center border-top border-dark"><p className={`fw-bold my-1 py-1 ${store.hidden ? "desenfoque" : ""}`}>{datos[13]?.valor}</p></div>
                         </div>
                         <div className="row">
                             <div className="col-4 text-center border-top border-dark"><p className={`fw-bold my-1 py-1 ${store.hidden ? "desenfoque" : ""}`}>3</p></div>
-                            <div className="col-2 text-center border-top border-dark"><p className={`fw-bold my-1 py-1 ${store.hidden ? "desenfoque" : ""}`}>{datos[8]?.valor}</p></div>
-                            <div className="col-2 text-center border-top border-dark"><p className={`fw-bold my-1 py-1 ${store.hidden ? "desenfoque" : ""}`}>{datos[9]?.valor}</p></div>
+                            <div className="col-2 text-center border-top border-dark"><p className={`fw-bold my-1 py-1 ${store.hidden ? "desenfoque" : ""}`}>{datos[2]?.valor}</p></div>
+                            <div className="col-2 text-center border-top border-dark"><p className={`fw-bold my-1 py-1 ${store.hidden ? "desenfoque" : ""}`}>{datos[6]?.valor}</p></div>
                             <div className="col-2 text-center border-top border-dark"><p className={`fw-bold my-1 py-1 ${store.hidden ? "desenfoque" : ""}`}>{datos[10]?.valor}</p></div>
-                            <div className="col-2 text-center border-top border-dark"><p className={`fw-bold my-1 py-1 ${store.hidden ? "desenfoque" : ""}`}>{datos[11]?.valor}</p></div>
+                            <div className="col-2 text-center border-top border-dark"><p className={`fw-bold my-1 py-1 ${store.hidden ? "desenfoque" : ""}`}>{datos[14]?.valor}</p></div>
                         </div>
                         <div className="row">
                             <div className="col-4 text-center border-top border-dark"><p className={`fw-bold my-1 py-1 ${store.hidden ? "desenfoque" : ""}`}>4</p></div>
-                            <div className="col-2 text-center border-top border-dark"><p className={`fw-bold my-1 py-1 ${store.hidden ? "desenfoque" : ""}`}>{datos[12]?.valor}</p></div>
-                            <div className="col-2 text-center border-top border-dark"><p className={`fw-bold my-1 py-1 ${store.hidden ? "desenfoque" : ""}`}>{datos[13]?.valor}</p></div>
-                            <div className="col-2 text-center border-top border-dark"><p className={`fw-bold my-1 py-1 ${store.hidden ? "desenfoque" : ""}`}>{datos[14]?.valor}</p></div>
+                            <div className="col-2 text-center border-top border-dark"><p className={`fw-bold my-1 py-1 ${store.hidden ? "desenfoque" : ""}`}>{datos[3]?.valor}</p></div>
+                            <div className="col-2 text-center border-top border-dark"><p className={`fw-bold my-1 py-1 ${store.hidden ? "desenfoque" : ""}`}>{datos[7]?.valor}</p></div>
+                            <div className="col-2 text-center border-top border-dark"><p className={`fw-bold my-1 py-1 ${store.hidden ? "desenfoque" : ""}`}>{datos[11]?.valor}</p></div>
                             <div className="col-2 text-center border-top border-dark"><p className={`fw-bold my-1 py-1 ${store.hidden ? "desenfoque" : ""}`}>{datos[15]?.valor}</p></div>
                         </div>
                     </div>
